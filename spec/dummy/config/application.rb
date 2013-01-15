d9fcf5610c6dce12c44e8522dfc2c095138f7eeb
Novikov Andrey <envek@envek.name>
2013-01-15 14:20:51 +1000

    Allow to use all supported rails versions in one app for testing.
    I've thrown DB=sqlite from .travis.yml as it seems not to be used anywhere.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,12 +1,17 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/version'
+if Rails::VERSION::MAJOR < 4
+  # Pick the frameworks you want:
+  require "active_record/railtie"
+  require "action_controller/railtie"
+  require "action_mailer/railtie"
+  require "active_resource/railtie"
+  require "sprockets/railtie"
+  # require "rails/test_unit/railtie"
+else
+  require 'rails/all'
+end
 
 Bundler.require
 require "axlsx_rails"
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,12 +2,12 @@ Dummy::Application.routes.draw do
   resources :users do
     resources :likes
   end
-  match "/users/:user_id/render_elsewhere(.:format)", :to => "likes#render_elsewhere"
-  match "/home(.:format)", :to => "home#index", :as => :home
-  match "/another(.:format)", :to => "home#another", :as => :another
-  match "/useheader(.:format)", :to => "home#useheader", :as => :useheader
-  match "/withpartial(.:format)", :to => "home#withpartial", :as => :withpartial
-  match "/home/render_elsewhere(.:format)", :to => "home#render_elsewhere"
-  match "/render_elsewhere(.:format)", :to => "home#render_elsewhere"
+  get "/users/:user_id/render_elsewhere(.:format)", :to => "likes#render_elsewhere"
+  get "/home(.:format)", :to => "home#index", :as => :home
+  get "/another(.:format)", :to => "home#another", :as => :another
+  get "/useheader(.:format)", :to => "home#useheader", :as => :useheader
+  get "/withpartial(.:format)", :to => "home#withpartial", :as => :withpartial
+  get "/home/render_elsewhere(.:format)", :to => "home#render_elsewhere"
+  get "/render_elsewhere(.:format)", :to => "home#render_elsewhere"
   root to: "home#index"
 end
