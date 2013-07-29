97b65698677f5b9105b727acd36e062cb9dddfb9
Noel Peden <noel@peden.biz>
2013-07-29 12:35:51 -0700

    Extended mail attachment testing.

--- spec/dummy/app/mailers/notifier.rb
+++ spec/dummy/app/mailers/notifier.rb
@@ -1,10 +1,26 @@
 class Notifier < ActionMailer::Base
-    default :from => 'noreply@company.com'
+  default :from => 'noreply@company.com'
+  # default :from => "Didier Leser - CESAM Nature <didier.leser@cesam-nature.com>"
 
-    def instructions(user)
-      @user = user
-      xlsx = render_to_string handlers: [:axlsx], template: 'users/mailers/instructions', layout: false
-      attachments["user_#{user.id}.pdf"] = {mime_type: Mime::XLSX, content: xlsx}
-      mail :to => user.email, :subject => 'Instructions'
-    end
+  def instructions(user)
+    @user = user
+
+    # normal syntax
+    xlsx = render handlers: [:axlsx], template: 'users/mailers/instructions', layout: false
+
+    # using render_to_string. sometimes not available, but why??
+    # xlsx = render_to_string handlers: [:axlsx], template: 'users/mailers/instructions', layout: false
+
+    # creating own view
+    # av = ActionView::Base.new()
+    # av.view_paths = ActionController::Base.view_paths
+    # av.extend ApplicationHelper
+    # av.assign user: @user
+    # xlsx = av.render handlers: [:axlsx], template: "users/mailers/instructions"
+    
+    attachments["user_#{user.id}.pdf"] = {mime_type: Mime::XLSX, content: xlsx}
+    mail :to => user.email, :subject => 'Instructions'
+  end
+
+
 end
