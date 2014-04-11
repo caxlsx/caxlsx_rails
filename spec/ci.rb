d024e6ed9a2731c3193652502e45369b7a138a2b
Noel Peden <noel@peden.biz>
2014-04-11 12:52:04 -0700

    Echo rails version

--- spec/ci.rb
+++ spec/ci.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 
 if ENV['RAILS_VERSION'] =~ /^4/
-  system('cd spec/dummy_4 && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec')
+  puts "Testing Rails 4"
+  system('echo $RAILS_VERSION; cd spec/dummy_4 && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec')
 else
-  system('cd spec/dummy && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec')
+  puts "Testing Rails 3"
+  system('echo $RAILS_VERSION; cd spec/dummy && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec')
 end
