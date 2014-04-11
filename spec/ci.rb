03eb8b4fba6645a677f050f6a603ce7b8fbd57c2
Noel Peden <noel@peden.biz>
2014-04-11 14:23:54 -0700

    Update scripts

--- spec/ci.rb
+++ spec/ci.rb
@@ -2,8 +2,8 @@
 
 if ENV['RAILS_VERSION'] =~ /^4/
   puts "Testing Rails 4"
-  system('echo $RAILS_VERSION; cd spec/dummy_4 && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec')
+  exit system('cd spec/dummy_4 && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec')
 else
   puts "Testing Rails 3"
-  system('echo $RAILS_VERSION; cd spec/dummy && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec')
+  exit system('cd spec/dummy && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec')
 end
