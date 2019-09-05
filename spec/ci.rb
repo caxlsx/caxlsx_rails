6fc3d02730aff0534273794252cec0849ce6ba35
Lucas Correa <lucas.correa@sysmatch.com>
2019-09-05 20:56:21 +0100

    Adjustments to dummy tests in rails versions 4 and 5

--- spec/ci.rb
+++ spec/ci.rb
@@ -7,8 +7,8 @@ elsif ENV['RAILS_VERSION'] =~ /^5/
   puts "Testing Rails #{ENV['RAILS_VERSION']}"
   exit system("cd spec/dummy_#{ENV['RAILS_VERSION']} && bundle install --without debug && rails db:reset && cd ../../ && bundle exec rspec spec")
 elsif ENV['RAILS_VERSION'] =~ /^4/
-  puts "Testing Rails #{ENV['RAILS_VERSION']}"
-  exit system("cd spec/dummy_#{ENV['RAILS_VERSION']} && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec")
+  puts "Testing Rails #{ENV['RAILS_VERSION'].to_i}"
+  exit system("cd spec/dummy_#{ENV['RAILS_VERSION'].to_i} && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec")
 else
   puts "Testing Rails 3"
   exit system('cd spec/dummy && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec')
