b61250b9cac2cb07dc3ace843eb17dc194c753e0
Lucas Correa <lucas.correa@sysmatch.com>
2019-09-05 20:41:43 +0100

    Adjustments to dummy tests in rails versions 5 and 6

--- spec/ci.rb
+++ spec/ci.rb
@@ -4,11 +4,11 @@ if ENV['RAILS_VERSION'] =~ /^6.0/
   puts "Testing Rails 6.0"
   exit system('cd spec/dummy_6.0 && bundle install --without debug && rails db:reset && cd ../../ && bundle exec rspec spec')
 elsif ENV['RAILS_VERSION'] =~ /^5/
-  puts "Testing Rails 5"
-  exit system('cd spec/dummy_5 && bundle install --without debug && rails db:reset && cd ../../ && bundle exec rspec spec')
+  puts "Testing Rails #{ENV['RAILS_VERSION']}"
+  exit system("cd spec/dummy_#{ENV['RAILS_VERSION']} && bundle install --without debug && rails db:reset && cd ../../ && bundle exec rspec spec")
 elsif ENV['RAILS_VERSION'] =~ /^4/
-  puts "Testing Rails 4"
-  exit system('cd spec/dummy_4 && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec')
+  puts "Testing Rails #{ENV['RAILS_VERSION']}"
+  exit system("cd spec/dummy_#{ENV['RAILS_VERSION']} && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec")
 else
   puts "Testing Rails 3"
   exit system('cd spec/dummy && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec')
