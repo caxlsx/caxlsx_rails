661f91e7c458c68d1089a902e176a3e862a485af
Noel Peden <noel@peden.biz>
2019-12-18 15:55:35 -0800

    Version 0.6.1, deprecation for axlsx_rails [skip ci]

--- lib/axlsx_rails.rb
+++ lib/axlsx_rails.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
+warn "DEPRECATION WARNING: axlsx_rails has been renamed to caxlsx_rails. See http://github.com/caxlsx"
+
 require 'axlsx_rails/railtie' if defined?(Rails)
