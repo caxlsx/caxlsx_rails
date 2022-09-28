223c660f79d05e1fc83165a46b2cc59c1ac60c04
Tim Diggins <tim@red56.uk>
2022-09-28 22:15:39 +0100

    potential fix for rails 4.2 github actions

--- spec/ci.rb
+++ spec/ci.rb
@@ -20,7 +20,8 @@ elsif ENV['RAILS_VERSION'] =~ /^5/
   exit system("cd spec/dummy_#{ENV['RAILS_VERSION']} && bundle config set --local without debug && bundle install && bundle exec rails db:reset && cd ../../ && bundle exec rspec spec")
 elsif ENV['RAILS_VERSION'] =~ /^4/
   puts "Testing Rails #{ENV['RAILS_VERSION'].to_i}"
-  exit system("cd spec/dummy_#{ENV['RAILS_VERSION'].to_i} && gem install bundler -v 1.17 && bundle _1.17_ install --without debug && bundle exec rake db:reset && cd ../../ && bundle exec rspec spec")
+  bundler1_version = "1.17.3"
+  exit system("cd spec/dummy_#{ENV['RAILS_VERSION'].to_i} && gem install bundler -v #{bundler1_version} && bundle _#{bundler1_version}_ install --without debug && bundle _#{bundler1_version}_ exec rake db:reset && cd ../../ && bundle _#{bundler1_version}_ exec rspec spec")
 else
   puts "Testing Rails 3"
   exit system('cd spec/dummy && bundle install --without debug && bundle exec rake db:create && bundle exec rake db:migrate && cd ../../ && bundle exec rspec spec')
