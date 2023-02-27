70e0298b343a5ffbf2bb8ee126b4adbbd90587bf
Koza <kiskoza@gmail.com>
2023-02-27 11:41:47 +0100

    Cleanup axlsx_builder_spec.rb

--- spec/axlsx_builder_spec.rb
+++ spec/axlsx_builder_spec.rb
@@ -1,48 +1,57 @@
 require 'spec_helper'
 
-describe 'Axlsx template handler' do
+describe AxlsxRails::TemplateHandler do
+  subject(:handler) { described_class.new }
 
-  AB = AxlsxRails::TemplateHandler
-  VT = Struct.new(:source)
-
-  let( :handler ) { AB.new }
-
-  let( :template ) do
-    VT.new("wb = xlsx_package.workbook;\nwb.add_worksheet(name: 'Test') do |sheet|\n\tsheet.add_row ['one', 'two', 'three']\n\tsheet.add_row ['a', 'b', 'c']\nend\n")
-  end
-
-  context "Rails #{Rails.version}" do
-    # for testing if the author is set
-    # before do
-      # Rails.stub_chain(:application, :config, :axlsx_author).and_return( 'Elmer Fudd' )
-    # end
-
+  describe '#default_format' do
     it "has xlsx format" do
       if Rails::VERSION::MAJOR >= 6
-        expect(handler.default_format).to eq(mime_type.symbol)
+        expect(handler.default_format).to eq(Mime[:xlsx].symbol)
+      elsif Rails::VERSION::MAJOR >= 5
+        expect(handler.default_format).to eq(Mime[:xlsx])
       else
-        expect(handler.default_format).to eq(mime_type)
+        expect(handler.default_format).to eq(Mime::XLSX)
+      end
+    end
+  end
+
+  describe 'how to compile it to an excel spreadsheet' do
+    let(:template) { Struct.new(:source).new(template_string) }
+
+    let(:template_string) { <<~RUBY }
+      wb = xlsx_package.workbook
+      wb.add_worksheet(name: 'Test') do |sheet|
+        sheet.add_row ['one', 'two', 'three']
+        sheet.add_row ['a', 'b', 'c']
+      end
+    RUBY
+
+    context 'when passing in a source' do
+      let(:source_string) { <<~RUBY }
+        wb = xlsx_package.workbook
+        wb.add_worksheet(name: 'Test') do |sheet|
+          sheet.add_row ['four', 'five', 'six']
+          sheet.add_row ['d', 'e', 'f']
+        end
+      RUBY
+
+      it "compiles to an excel spreadsheet when passing in a source" do
+        xlsx_package, wb = nil
+        eval(described_class.new.call(template, source_string))
+        xlsx_package.serialize('/tmp/axlsx_temp.xlsx')
+        expect { wb = Roo::Excelx.new('/tmp/axlsx_temp.xlsx') }.to_not raise_error
+        expect(wb.cell(2,3)).to eq('f')
       end
     end
 
-    it "compiles to an excel spreadsheet when passing in a source" do
-      xlsx_package, wb = nil
-      source = "wb = xlsx_package.workbook;\nwb.add_worksheet(name: 'Test') do |sheet|\n\tsheet.add_row ['four', 'five', 'six']\n\tsheet.add_row ['d', 'e', 'f']\nend\n"
-      eval( AB.new.call template, source )
-      xlsx_package.serialize('/tmp/axlsx_temp.xlsx')
-      expect{ wb = Roo::Excelx.new('/tmp/axlsx_temp.xlsx') }.to_not raise_error
-      expect(wb.cell(2,3)).to eq('f')
+    context 'when not passing in a source' do
+      it "compiles to an excel spreadsheet when inferring source from template " do
+        xlsx_package, wb = nil
+        eval(described_class.new.call(template))
+        xlsx_package.serialize('/tmp/axlsx_temp.xlsx')
+        expect { wb = Roo::Excelx.new('/tmp/axlsx_temp.xlsx') }.to_not raise_error
+        expect(wb.cell(2,3)).to eq('c')
+      end
     end
-
-    it "compiles to an excel spreadsheet when inferring source from template " do
-      xlsx_package, wb = nil
-      eval( AB.new.call template )
-      xlsx_package.serialize('/tmp/axlsx_temp.xlsx')
-      expect{ wb = Roo::Excelx.new('/tmp/axlsx_temp.xlsx') }.to_not raise_error
-      expect(wb.cell(2,3)).to eq('c')
-    end
-
-    #TODO:
-    # Test if author field is set - does roo parse that?
   end
 end
